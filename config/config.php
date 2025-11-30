cec59a8d6aa3c2b0fc0ce04abf2a27a607903819
Dono-lab <doxtrem@outlook.com>
2025-11-30 18:15:20 +0100

    Nettoyage

--- config/config.php
+++ config/config.php
@@ -4,15 +4,13 @@ $user = "root";
 $password = "";
 $database = "projet_profit";
 
-// Durée de vie de la session en secondes (30 minutes)
 define('SESSION_LIFETIME', 1800);
 
-// Configuration SMTP (OVH)
-define('SMTP_HOST', 'ssl0.ovh.net'); // Serveur OVH standard
-define('SMTP_PORT', 465); // Port SSL
-define('SMTP_USER', 'votre-email@votre-domaine.com'); // VOTRE ADRESSE EMAIL COMPLETE
-define('SMTP_PASS', 'votre-mot-de-passe-mail'); // LE MOT DE PASSE DE CETTE ADRESSE EMAIL
-define('SMTP_FROM_EMAIL', 'no-reply@prof-it.com'); // Doit souvent être identique à SMTP_USER chez OVH
+define('SMTP_HOST', 'ssl0.ovh.net');
+define('SMTP_PORT', 465);
+define('SMTP_USER', 'support@prof-it.fr');
+define('SMTP_PASS', 'Support2025!');
+define('SMTP_FROM_EMAIL', 'support@prof-it.fr');
 define('SMTP_FROM_NAME', 'Prof-IT Notification');
 
 try {
